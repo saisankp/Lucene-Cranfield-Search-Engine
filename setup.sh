83bd5511a9843f7487765a0365e2a0812464530b
saisankp <prathameshsaisankar@Gmail.com>
2023-10-23 16:48:32 +0100

    update scripts for linux

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ mkdir -p cranfield-collection
 cd cranfield-collection
 curl -L https://ir.dcs.gla.ac.uk/resources/test_collections/cran/cran.tar.gz | tar xz
 cd ..
+[[ -d trec_eval-9.0.7 ]] && rm -r trec_eval-9.0.7
 curl -L https://trec.nist.gov/trec_eval/trec_eval-9.0.7.tar.gz | tar xz
 cd trec_eval-9.0.7
 make
